8ec832b40a354a7f0a50583608eab576f7c3a286
Monyairitshidze <140395495+Monyairitshidze@users.noreply.github.com>
2024-11-04 01:55:11 +0200

    Update OnlineInventoryStore.java

--- onlineInventoryStore/src/onlineinventorystore/OnlineInventoryStore.java
+++ onlineInventoryStore/src/onlineinventorystore/OnlineInventoryStore.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class OnlineInventoryStore {
 
-    
+  //write code which allow the user to add products and store them on the linkedlist  
     public static void main(String[] args) {
         
        Products product = new Products();
@@ -15,11 +15,13 @@ public class OnlineInventoryStore {
         //ask the user if they want to add product
         System.out.println("DO YOU WANT TO ADD PRODUCTS???");
         String choice = sc.next().toLowerCase();
-        
+
+        //ask the user the number of products they want to add
         System.out.println("HOW MANY PRODUCTS YOU WANNA ADD PRODUCTS???");
         int number = sc.nextInt();
         if(choice.equalsIgnoreCase("yes")){
-          
+
+            //if the condition is met this loop we loop for the specified number
             for(int i=0; i< number; i++){
                 System.out.println("ENTER NAME OF PRODUCT");
                 String name = sc.next().toUpperCase();
@@ -29,13 +31,16 @@ public class OnlineInventoryStore {
                 
                 System.out.println("ENTER SHELF TO STORE PRODUCTS");
                 int shelf = sc.nextInt();
-                
+
+                //we all the add method to add products on the arraylist
                 product.addProducts(name, price,shelf);
             }
-            
+
+            //we ask the user if they want to check if the products are added
             System.out.println("DO YOU WANNA CHECK IF THE PRODUCTS ARE ADDED???");
             String choice3 = sc.next().toLowerCase();
-            
+
+            //if yes we call display method
             if(choice.equalsIgnoreCase("yes")){
                 
                 product.display();
